7b3033871f2615cac854a1092c90beeb9703137e
Roman Luchka <luchkaroman@gmail.com>
2019-02-27 17:00:44 +0200

    display pressed number keys

--- main.js
+++ main.js
@@ -1,13 +1,21 @@
 const calculator = document.querySelector('.calculator');
 const keys = document.querySelector('.calculator__keys');
-
+const display = document.querySelector('.calculator__display');
 
 keys.addEventListener('click', e => {
 	if (e.target.matches('button')) {
 		const key = e.target;
 		const action = key.dataset.action;
+		const keyContent = key.textContent;
+		const displayedNum = display.textContent;
+
 		if (!action) {
 			console.log('number key!')
+			if (displayedNum === '0') {
+				display.textContent = keyContent
+			} else {
+				display.textContent = displayedNum + keyContent
+			}
 		}
 		if (action === 'add' ||
 			action === 'subtract' ||
@@ -17,6 +25,7 @@ keys.addEventListener('click', e => {
 		}
 		if (action === 'decimal') {
 			console.log('decimal key!')
+			display.textContent = displayedNum + keyContent
 		}
 		if (action === 'clear') {
 			console.log('clear key!')
@@ -24,7 +33,6 @@ keys.addEventListener('click', e => {
 		if (action === 'calculate') {
 			console.log('equal key!')
 		}
-
 	}
 });
 
